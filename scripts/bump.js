b0ce8590b340eb4de4f13aa8f1f750205eb3a03e
Nathan Bishop <nbish11@hotmail.com>
2016-07-22 05:50:20 +1000

    Fix path pattern to include all source files

--- scripts/bump.js
+++ scripts/bump.js
@@ -11,7 +11,7 @@ var newVersion = JSON.parse(fs.readFileSync('./package.json')).version;
 sed('-i', /\ \ "version": "(.*)",/, '  "version": "' + newVersion + '",', 'bower.json');
 
 // Update version tag in docblocks
-sed('-i', /\ * @version .*/, ' @version ' + newVersion, 'src/js/Moovie.js');
+sed('-i', /\ * @version .*/, ' @version ' + newVersion, 'src/js/*.js');
 
 // Success
 exit(0);
