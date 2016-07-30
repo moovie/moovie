bf55c0995419e8e8caf78ddd1c529cb0b16d159c
Nathan Bishop <nbish11@hotmail.com>
2016-07-30 11:19:46 +1000

    Separate media events into own module

---
+++ src/js/Moovie.MediaEvents.js
@@ -0,0 +1,35 @@
+/**
+ * Moovie: an advanced HTML5 video player for MooTools.
+ *
+ * Currently supported HTML5 media events.
+ *
+ * @version 0.3.1
+ * @author Colin Aarts <colin@colinaarts.com> (http://colinaarts.com)
+ * @author Nathan Bishop <nbish11@hotmail.com>
+ * @copyright 2010 Colin Aarts
+ * @license MIT
+ */
+Moovie.MediaEvents = {  // eslint-disable-line
+    abort: 1,
+    canplay: 1,
+    canplaythrough: 1,
+    durationchange: 1,
+    emptied: 1,
+    ended: 1,
+    error: 1,
+    loadeddata: 1,
+    loadedmetadata: 1,
+    loadstart: 1,
+    pause: 1,
+    play: 1,
+    playing: 1,
+    progress: 2,    // @todo change to "1"
+    ratechange: 1,
+    seeked: 1,
+    seeking: 1,
+    stalled: 1,
+    suspend: 1,
+    timeupdate: 1,
+    volumechange: 1,
+    waiting: 1
+};
--- src/js/Moovie.js
+++ src/js/Moovie.js
@@ -26,7 +26,7 @@ var Moovie = function(videos, options) {
 
     // Add HTML 5 media events to Element.NativeEvents, if needed.
     if (!Element.NativeEvents.timeupdate) {
-        Element.NativeEvents = Object.merge({ abort: 1, canplay: 1, canplaythrough: 1, durationchange: 1, emptied: 1, ended: 1, loadeddata: 1, loadedmetadata: 1, loadstart: 1, pause: 1, play: 1, playing: 1, progress: 2, ratechange: 1, seeked: 1, seeking: 1, stalled: 1, suspend: 1, timeupdate: 1, volumechange: 1, waiting: 1 }, Element.NativeEvents);
+        Element.NativeEvents = Object.merge(Element.NativeEvents, Moovie.MediaEvents);
     }
 
     videos.each(function(el) {
