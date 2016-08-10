57a5cb99d9141325ea8df0af5714d2d7e0efb77a
Nathan Bishop <nbish11@hotmail.com>
2016-08-10 21:57:02 +1000

    Fix bug in version bump

--- scripts/bump.js
+++ scripts/bump.js
@@ -11,7 +11,8 @@ var newVersion = JSON.parse(fs.readFileSync('./package.json')).version;
 sed('-i', /\ \ "version": "(.*)",/, '  "version": "' + newVersion + '",', 'bower.json');
 
 // Update version tag in docblocks
-sed('-i', /\ * @version .*/, ' @version ' + newVersion, 'src/**/*');
+sed('-i', /\ * @version .*/, ' @version ' + newVersion, 'src/js/*.js');
+sed('-i', /\ * @version .*/, ' @version ' + newVersion, 'src/scss/main.scss');
 
 // Success
 exit(0);
