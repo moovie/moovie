107e674e1dbc433b6f130ac6139273ea4145f753
Nathan Bishop <nbish11@hotmail.com>
2016-08-15 01:36:14 +1000

    0.4.3

--- src/js/texttracks/TextTrackMode.js
+++ src/js/texttracks/TextTrackMode.js
@@ -3,7 +3,7 @@
  *
  * Enum for the W3C TextTrackMode IDL.
  *
- * @version 0.4.2
+ * @version 0.4.3
  * @author Colin Aarts <colin@colinaarts.com> (http://colinaarts.com)
  * @author Nathan Bishop <nbish11@hotmail.com>
  * @copyright 2010 Colin Aarts
