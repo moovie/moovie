16c664d98e6e1a50b79867a9bc6b7664506558d8
Nathan Bishop <nbish11@hotmail.com>
2016-08-18 09:04:21 +1000

    0.4.5

--- src/js/texttracks/TextTrackKind.js
+++ src/js/texttracks/TextTrackKind.js
@@ -3,7 +3,7 @@
  *
  * Enum for the W3C TextTrackKind IDL.
  *
- * @version 0.4.4
+ * @version 0.4.5
  * @author Colin Aarts <colin@colinaarts.com> (http://colinaarts.com)
  * @author Nathan Bishop <nbish11@hotmail.com>
  * @copyright 2010 Colin Aarts
