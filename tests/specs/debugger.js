66848ed5df57c1274d185f674a6c2b58a86e1ac6
Nathan Bishop <nbish11@hotmail.com>
2016-09-10 15:46:29 +1000

    refactor: Remove lowercased files

--- tests/specs/debugger.js
+++
@@ -1,7 +0,0 @@
-import Debugger from '../../src/js/Debugger.js';
-
-describe('Debugger', function () {
-    it('needs to have tests written', function () {
-        expect(Debugger).toBeDefined();
-    });
-});
