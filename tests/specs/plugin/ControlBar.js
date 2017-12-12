2e6b17d9655b168f8c5e985ebedd414405ff22e8
Nathan Bishop <nbish11@hotmail.com>
2017-12-12 21:21:17 +1000

    tests: add test support for ControlBar plugin

---
+++ tests/specs/plugin/ControlBar.js
@@ -0,0 +1,8 @@
+import expect from 'expectations';
+import ControlBar from '../../../src/js/plugin/ControlBar.js';
+
+describe('ControlBar', function () {
+    it('is exported', function () {
+        expect(ControlBar).toBeDefined();
+    });
+});
