524469901ea87f3a49daf77f4bb83565658ee512
Nathan Bishop <nbish11@hotmail.com>
2017-12-12 23:07:03 +1000

    feat: add Button component

---
+++ src/js/component/Button.js
@@ -0,0 +1,94 @@
+/**
+ * Moovie: an advanced HTML5 video player for MooTools.
+ * @copyright 2010 Colin Aarts
+ * @license MIT
+ */
+import Component from '../core/Component.js';
+
+/**
+ * UI component for creating aria-enabled button elements.
+ * @class
+ */
+const Button = new Class({
+    Extends: Component,
+
+    /**
+     * @inheritdoc
+     */
+    options: {},
+
+    /**
+     * The label for the button.
+     * @readonly
+     * @type {String}
+     */
+    label: '',
+
+    /**
+     * The function to call when the button is clicked.
+     * @readonly
+     * @type {Function}
+     */
+    action: Function.from(),
+
+    /**
+     * @inheritdoc
+     * @param {String} label The button's label.
+     * @param {Function} action The function to call when the button is clicked.
+     * @param {Object} options Additional configuration options.
+     */
+    initialize: function (label, action, options) {
+        if (typeOf(label) !== 'string') {
+            throw new TypeError('`label` must be a string');
+        }
+
+        if (typeOf(action) !== 'function') {
+            throw new TypeError('`action` must be a function');
+        }
+
+        this.label = label;
+        this.action = action;
+        this.parent(options);
+    },
+
+    /**
+     * @inheritdoc
+     */
+    build: function () {
+        return new Element('button', {
+            'class': 'moovie-button',
+            'aria-label': this.label
+        });
+    },
+
+    /**
+     * Attach event listeners.
+     * @return {Button} The current instance for method chaining.
+     */
+    attach: function () {
+        document.id(this).addEvent('click', this.getBound('click'));
+
+        return this;
+    },
+
+    /**
+     * Detach event listeners.
+     * @return {Button} The current instance for method chaining.
+     */
+    detach: function () {
+        document.id(this).removeEvent('click', this.getBound('click'));
+
+        return this;
+    },
+
+    /**
+     * Handles the "click" event.
+     */
+    click: function () {
+        this.action.call(this, this);
+    }
+});
+
+Component.register('button', Button);
+
+export default Button;
---
+++ tests/specs/component/Button.js
@@ -0,0 +1,108 @@
+import expect from 'expectations';
+import sinon from 'sinon';
+import Button from '../../../src/js/component/Button.js';
+import Component from '../../../src/js/core/Component.js';
+
+describe('Button', function () {
+    it('gets exported as a default module', function () {
+        expect(Button).toBeDefined();
+    });
+
+    it('extends the `Component` base class', function () {
+        const testButton = new Button('Test button', Function.from());
+
+        expect(instanceOf(testButton, Component)).toBe(true);
+    });
+
+    it('is a registered `Component`', function () {
+        const exists = Object.keyOf(Component.registered, Button);
+
+        expect(exists).toEqual('button');
+    });
+
+    context('when creating a new instance', function () {
+        it('throws a TypeError if the label is not a string', function () {
+            const testButton = function () {
+                return new Button(4, Function.from());
+            };
+
+            expect(testButton).toThrow('`label` must be a string');
+        });
+
+        it('has a label', function () {
+            const label = 'Test button';
+            const testButton = new Button(label, Function.from());
+            const buttonElement = document.id(testButton);
+
+            expect(testButton.label).toEqual(label);
+            expect(buttonElement.get('aria-label')).toEqual(label);
+        });
+
+        it('throws a TypeError if action is not a function', function () {
+            const testButton = function () {
+                return new Button('Test button', 'a function');
+            };
+
+            expect(testButton).toThrow('`action` must be a function');
+        });
+
+        it('is enabled by default', function () {
+            const testButton = new Button('Test button', Function.from());
+            const buttonElement = document.id(testButton);
+
+            expect(testButton.disabled).toBe(false);
+            expect(buttonElement.get('aria-disabled')).toEqual('false');
+        });
+
+        it('is visible by default', function () {
+            const testButton = new Button('Test button', Function.from());
+            const buttonElement = document.id(testButton);
+
+            expect(testButton.hidden).toBe(false);
+            expect(buttonElement.hasAttribute('hidden')).toBe(false);
+        });
+
+        it('has the correct tag', function () {
+            const testButton = new Button('Test button', Function.from());
+            const buttonElement = document.id(testButton);
+
+            expect(buttonElement.get('tag')).toEqual('button');
+        });
+
+        it('has the correct class', function () {
+            const testButton = new Button('Test button', Function.from());
+            const buttonElement = document.id(testButton);
+
+            expect(buttonElement.hasClass('moovie-button')).toBe(true);
+        });
+    });
+
+    context('when clicking the button', function () {
+        it('calls the specified action', function () {
+            const action = sinon.spy();
+            const testButton = new Button('Test button', action);
+
+            testButton.click();
+
+            expect(action.called).toBe(true);
+        });
+
+        it('gets passed the button as an argument', function () {
+            const action = sinon.spy();
+            const testButton = new Button('Test button', action);
+
+            testButton.click();
+
+            expect(action.calledWith(testButton)).toBe(true);
+        });
+
+        it('provides the button as the action\'s context', function () {
+            const action = sinon.spy();
+            const testButton = new Button('Test button', action);
+
+            testButton.click();
+
+            expect(action.calledOn(testButton)).toBe(true);
+        });
+    });
+});
