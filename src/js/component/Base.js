a3e1c9edb5366042b34390e73b97ed21451f4dee
Nathan Bishop <nbish11@hotmail.com>
2018-06-24 17:03:10 +1000

    refactor: No longer depends on MooTools

--- src/js/component/Base.js
+++ src/js/component/Base.js
@@ -3,54 +3,34 @@
  * @copyright 2010 Colin Aarts
  * @license MIT
  */
+import EventTarget from 'event-target-shim';
 
-/**
- * Base class that all UI components must extend.
- * @abstract
- * @class
- */
-const Base = new Class({
-    Implements: [Events, Options],
+const disabled = Symbol('disabled');
+const hidden = Symbol('hidden');
+const element = Symbol('element');
+const bound = Symbol('bound');
+const defaultOptions = {
+    disabled: false,
+    hidden: false
+};
 
-    /**
-     * Default options that are merged in with the constructor options.
-     * @protected
-     * @type {Object}
-     */
-    options: {
-        disabled: false,
-        hidden: false
-    },
-
+export default class Base extends EventTarget
+{
     /**
      * Indicates when the component is in the "disabled" state.
-     * @readonly
      * @type {Boolean}
      */
-    disabled: false,
+    get disabled() {
+        return this[disabled]
+    }
 
     /**
      * Indicates when the component is in the "hidden" state.
-     * @readonly
      * @type {Boolean}
      */
-    hidden: false,
-
-    /**
-     * What is returned when the sub-component is used as an element.
-     * @private
-     * @readonly
-     * @type {Element}
-     */
-    element: null,
-
-    /**
-     * Methods that are bound to the `this` instance.
-     * @private
-     * @readonly
-     * @type {Object}
-     */
-    bound: {},
+    get hidden() {
+        return this[hidden];
+    }
 
     /**
      * Initializes the child class into a working state. This
@@ -61,17 +41,17 @@ const Base = new Class({
      * @param  {Object} options Additional options to configure instance.
      * @return {Base} The constructed class.
      */
-    initialize: function (options) {
-        this.setOptions(options);
-        this.element = this.build();
+    constructor(options = defaultOptions) {
+        super();
+        this[element] = this.build();
 
-        if (!this.element) {
+        if (!this[element]) {
             throw new Error('The `build()` must return an element.');
         }
 
-        this[this.options.disabled ? 'disable' : 'enable']();
-        this[this.options.hidden ? 'hide' : 'show']();
-    }.protect(),
+        this[options.disabled ? 'disable' : 'enable']();
+        this[options.hidden ? 'hide' : 'show']();
+    }
 
     /**
      * Create the component's DOM structure.
@@ -79,73 +59,73 @@ const Base = new Class({
      * @protected
      * @return {Element} The base element of the component's structure.
      */
-    build: function () {
+    build() {
         throw new Error('The `build()` method must be implemented by a child class.');
-    }.protect(),
+    }
 
     /**
      * Enables the component.
      * @return {Base} The current instance for method chaining.
      */
-    enable: function () {
-        this.disabled = false;
-        this.element.set('aria-disabled', false);
+    enable() {
+        this[disabled] = false;
+        this[element].set('aria-disabled', false);
 
-        if (typeOf(this.attach) === 'function') {
+        if (typeof this.attach === 'function') {
             this.attach();
         }
 
         return this;
-    },
+    }
 
     /**
      * Disables the component.
      * @return {Base} The current instance for method chaining.
      */
-    disable: function () {
-        this.disabled = true;
-        this.element.set('aria-disabled', true);
+    disable() {
+        this[disabled] = true;
+        this[element].set('aria-disabled', true);
 
-        if (typeOf(this.detach) === 'function') {
+        if (typeof this.detach === 'function') {
             this.detach();
         }
 
         return this;
-    },
+    }
 
     /**
      * Shows the element in the DOM.
      * @fires Base#show
      * @return {Base} The current instance for method chaining.
      */
-    show: function () {
-        this.hidden = false;
-        this.element.erase('hidden');
-        this.fireEvent('show');
+    show() {
+        this[hidden] = false;
+        this[element].removeAttribute('hidden');
+        this.dispatchEvent(new CustomEvent('show'));
 
         return this;
-    },
+    }
 
     /**
      * Hides the element in the DOM.
      * @fires Base#hide
      * @return {Base} The current instance for method chaining.
      */
-    hide: function () {
-        this.hidden = true;
-        this.element.set('hidden', '');
-        this.fireEvent('hide');
+    hide() {
+        this[hidden] = true;
+        this[element].setAttribute('hidden', '');
+        this.dispatchEvent(new CustomEvent('hide'));
 
         return this;
-    },
+    }
 
     /**
      * Converts the class to an element representation for use in DOM operations.
      * @return {Element} The element created by the `build()` method.
      */
-    toElement: function () {
-        return this.element;
-    },
+    toElement() {
+        return this[element];
+    }
 
     /**
      * Retrieves a function bound to the instance. Use with event listeners
@@ -159,13 +139,11 @@ const Base = new Class({
      * @param  {String} methodName The name of the bound method.
      * @return {Function} The bound method.
      */
-    getBound: function (methodName) {
-        if (!(methodName in this.bound)) {
-            this.bound[methodName] = this[methodName].bind(this);
+    getBound(methodName) {
+        if (!(methodName in this[bound])) {
+            this[bound][methodName] = this[methodName].bind(this);
         }
 
-        return this.bound[methodName];
-    }.protect()
-});
-
-export default Base;
+        return this[bound][methodName];
+    }
+}
