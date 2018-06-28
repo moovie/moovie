6383f620a1da68f30ba7d1b5fa4fdb2e6611146f
Nathan Bishop <nbish11@hotmail.com>
2018-06-28 16:04:57 +1000

    refactor: Remove dependence on MooTools

--- src/js/component/Tooltip.js
+++ src/js/component/Tooltip.js
@@ -5,85 +5,87 @@
  */
 import Base from './Base.js';
 
+const symbolForTarget = Symbol('target');
+const defaultOptions = {
+    hidden: true,
+
+    // 'none' || 'x' || 'y' || 'both'
+    axis: 'both',
+
+    // called everytime the tooltip is shown
+    content: function () {
+        return this.target.getAttribute('aria-label');
+    }
+};
+
 /**
  * UI component for creating aria-enabled tooltip elements.
  * @class
  */
-const Tooltip = new Class({
-    Extends: Base,
-
-    /**
-     * @inheritdoc
-     */
-    options: {
-        hidden: true,
-
-        // 'none' || 'x' || 'y' || 'both'
-        axis: 'both',
-
-        // called everytime the tooltip is shown
-        content: function (element) {
-            return element.get('aria-label');
-        }
-    },
+export default class Tooltip extends Base {
+    get target() {
+        return this[symbolForTarget];
+    }
 
     /**
      * @inheritdoc
      * @param {Element} target The element to use as the trigger for the tooltip.
      */
-    initialize: function (target, options) {
-        this.target = document.id(target);
-        this.parent(options);
-    },
+    constructor(target, options = defaultOptions) {
+        super(options);
+        this[symbolForTarget] = target;
+        this[this.options.disabled ? 'disable' : 'enable']();
+        this[this.options.hidden ? 'hide' : 'show']();
+    }
 
     /**
      * @inheritdoc
      */
-    build: function () {
+    build() {
         return new Element('div', {
             class: 'moovie-tooltip',
             role: 'tooltip'
         });
-    },
+    }
 
     /**
      * Attach event listeners.
      * @return {Tooltip} The current instance for method chaining.
      */
-    attach: function () {
-        this.target.addEvent('mousemove', this.getBound('update'));
-        this.target.addEvent('mouseleave', this.getBound('hide'));
+    attach() {
+        this[symbolForTarget].addEventListener('mousemove', this.getBound('update'));
+        this[symbolForTarget].addEventListener('mouseleave', this.getBound('hide'));
 
         return this;
-    },
+    }
 
     /**
      * Detach event listeners.
      * @return {Tooltip} The current instance for method chaining.
      */
-    detach: function () {
-        this.target.removeEvent('mousemove', this.getBound('update'));
-        this.target.removeEvent('mouseleave', this.getBound('hide'));
+    detach() {
+        this[symbolForTarget].removeEventListener('mousemove', this.getBound('update'));
+        this[symbolForTarget].removeEventListener('mouseleave', this.getBound('hide'));
 
         return this;
-    },
+    }
 
     /**
      * Handles the "mousemove" event.
      * @param  {Object} event The "mousemove" event.
      */
-    update: function (event) {
+    update(event) {
         const content = this.options.content.call(this, event.target);
 
         if (content) {
-            this.element.set('html', content);
+            this.toElement().set('html', content);
 
             if (['x', 'both'].contains(this.options.axis)) {
-                this.element.setStyle('left', event.page.x - this.target.getLeft());
+                this.toElement().setStyle('left', event.page.x - this[symbolForTarget].getLeft());
             }
 
             if (['y', 'both'].contains(this.options.axis)) {
-                this.element.setStyle('top', event.page.y - this.target.getTop());
+                this.toElement().setStyle('top', event.page.y - this[symbolForTarget].getTop());
             }
 
             this.show();
@@ -91,6 +93,4 @@ const Tooltip = new Class({
             this.hide();
         }
     }
-});
-
-export default Tooltip;
+}
