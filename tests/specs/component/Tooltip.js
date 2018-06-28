56fff012b576e620c76e54eca86e3ff9ef98d659
Nathan Bishop <nbish11@hotmail.com>
2018-06-28 16:04:30 +1000

    tests: Add some tests

--- tests/specs/component/Tooltip.js
+++ tests/specs/component/Tooltip.js
@@ -11,6 +11,43 @@ describe('Tooltip', function () {
         const target = new Element('button');
         const tooltip = new Tooltip(target);
 
-        expect(instanceOf(tooltip, Base)).toEqual(true);
+        expect(tooltip instanceof Base).toBe(true);
+    });
+
+    context('when creating a new instance', function () {
+        it('is hidden by default', function () {
+            const button = new Element('button');
+            const tooltip = new Tooltip(button);
+            const element = tooltip.toElement();
+
+            expect(tooltip.hidden).toBe(true);
+            expect(element.hasAttribute('hidden')).toBe(true);
+        });
+
+        it('sets the correct default options', function () {
+            const button = new Element('button');
+            const tooltip = new Tooltip(button);
+
+            expect(tooltip.options.hidden).toBe(true);
+            expect(tooltip.options.axis).toEqual('both');
+            expect(tooltip.options.content).toBeDefined();
+            expect(typeof tooltip.options.content).toEqual('function');
+        });
+
+        it('creates the element with the correct CSS class', function () {
+            const button = new Element('button');
+            const tooltip = new Tooltip(button);
+            const element = tooltip.toElement();
+
+            expect(element.hasClass('moovie-tooltip')).toBe(true);
+        });
+
+        it('creates the element with the correct role', function () {
+            const button = new Element('button');
+            const tooltip = new Tooltip(button);
+            const element = tooltip.toElement();
+
+            expect(element.getAttribute('role')).toEqual('tooltip');
+        });
     });
 });
